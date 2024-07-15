79fdde0cf00a89a227642e16cd25e16131fbff6a
DeepT. <43941768+DeepTMods@users.noreply.github.com>
2024-07-15 22:58:38 +0800

    add(cli): add support for command line

--- mhw_witcher_fix.py
+++ mhw_witcher_fix.py
@@ -1,3 +1,4 @@
+import argparse
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import webbrowser
@@ -22,6 +23,16 @@ def fix_slot_data(data):
     return data
 
 
+def modify_file(input_file_path, output_file_path, status_var):
+    with open(input_file_path, "rb") as file:
+        data = bytearray(file.read())
+
+    data = fix_slot_data(data)
+
+    with open(output_file_path, "wb") as file:
+        file.write(data)
+
+
 def open_file_dialog(status_var):
     input_file_path = filedialog.askopenfilename()
     with open(input_file_path, "rb") as file:
@@ -41,37 +52,47 @@ def open_file_dialog(status_var):
 
 
 def main():
-    root = tk.Tk()
-    root.title("MHW witcher side quest Fixer")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file_path", help="source file", nargs="?")
+    parser.add_argument("output_file_path", help="fix file", nargs="?")
+    args = parser.parse_args()
 
-    label = tk.Label(
-        root,
-        text="This is a .mhwslot data fixing tool for fixing Witcher side quest missing.",
-    )
-    label.pack()
+    if args.input_file_path and args.output_file_path:
+        modify_file(args.input_file_path, args.output_file_path)
+    else:
+        root = tk.Tk()
+        root.title("MHW witcher side quest Fixer")
 
-    button = tk.Button(
-        root,
-        text="Choose HMW Slot Data",
-        command=lambda: open_file_dialog(status_var),
-    )
-    button.pack()
+        label = tk.Label(
+            root,
+            text="This is a .mhwslot data fixing tool for fixing Witcher side quest missing.",
+        )
+        label.pack()
 
-    link = tk.Label(root, text="GitHub", fg="blue", cursor="hand2")
-    link.pack()
-    link.bind(
-        "<Button-1>",
-        lambda e: webbrowser.open_new("https://github.com/DeepTMods/mhw_witcher_fixer"),
-    )
+        button = tk.Button(
+            root,
+            text="Choose HMW Slot Data",
+            command=lambda: open_file_dialog(status_var),
+        )
+        button.pack()
 
-    status_var = tk.StringVar()
-    status_var.set("v0.1.0, by DeepT. @ DeepTMods(DTM)")
-    status_bar = tk.Label(
-        root, textvariable=status_var, bd=1, relief=tk.SUNKEN, anchor=tk.W
-    )
-    status_bar.pack(side=tk.BOTTOM, fill=tk.X)
+        link = tk.Label(root, text="GitHub", fg="blue", cursor="hand2")
+        link.pack()
+        link.bind(
+            "<Button-1>",
+            lambda e: webbrowser.open_new(
+                "https://github.com/DeepTMods/mhw_witcher_fix"
+            ),
+        )
 
-    root.mainloop()
+        status_var = tk.StringVar()
+        status_var.set("v0.1, by DeepT. @ DeepTMods(DTM)")
+        status_bar = tk.Label(
+            root, textvariable=status_var, bd=1, relief=tk.SUNKEN, anchor=tk.W
+        )
+        status_bar.pack(side=tk.BOTTOM, fill=tk.X)
+
+        root.mainloop()
 
 
 if __name__ == "__main__":
