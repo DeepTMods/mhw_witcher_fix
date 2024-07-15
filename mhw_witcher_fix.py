b8bfecc029c7d65fe583e143901354f1a2fd09ef
DeepT. <43941768+DeepTMods@users.noreply.github.com>
2024-07-15 22:42:57 +0800

    add(gui): add gui

--- mhw_witcher_fix.py
+++ mhw_witcher_fix.py
@@ -1,3 +1,8 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import webbrowser
+
+
 def fix_slot_data(data):
     search_bytes = bytearray.fromhex("15000000D81812CE")
     replace_bytes = bytearray.fromhex("FFFFFFFF00000000")
@@ -15,3 +20,59 @@ def fix_slot_data(data):
     data[index] &= 0b11111011
 
     return data
+
+
+def open_file_dialog(status_var):
+    input_file_path = filedialog.askopenfilename()
+    with open(input_file_path, "rb") as file:
+        data = bytearray(file.read())
+
+    data = fix_slot_data(data)
+
+    messagebox.showinfo("Processed!", "Please choose where to save fixed data.")
+
+    output_file_path = filedialog.asksaveasfilename(
+        defaultextension=".mhwslot", filetypes=[("MHW Slot Data", "*.mhwslot")]
+    )
+    with open(output_file_path, "wb") as file:
+        file.write(data)
+
+    messagebox.showinfo("Fixed file saved ", "Fixed file saved at: " + output_file_path)
+
+
+def main():
+    root = tk.Tk()
+    root.title("MHW witcher side quest Fixer")
+
+    label = tk.Label(
+        root,
+        text="This is a .mhwslot data fixing tool for fixing Witcher side quest missing.",
+    )
+    label.pack()
+
+    button = tk.Button(
+        root,
+        text="Choose HMW Slot Data",
+        command=lambda: open_file_dialog(status_var),
+    )
+    button.pack()
+
+    link = tk.Label(root, text="GitHub", fg="blue", cursor="hand2")
+    link.pack()
+    link.bind(
+        "<Button-1>",
+        lambda e: webbrowser.open_new("https://github.com/DeepTMods/mhw_witcher_fixer"),
+    )
+
+    status_var = tk.StringVar()
+    status_var.set("v0.1.0, by DeepT. @ DeepTMods(DTM)")
+    status_bar = tk.Label(
+        root, textvariable=status_var, bd=1, relief=tk.SUNKEN, anchor=tk.W
+    )
+    status_bar.pack(side=tk.BOTTOM, fill=tk.X)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
